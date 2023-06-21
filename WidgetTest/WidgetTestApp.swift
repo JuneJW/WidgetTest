3917cdd92f1c78821d16d0f8fae74a9acfb581b2
JuneJW <juneyjw@gmail.com>
2023-06-21 14:03:49 +0900

    Initial Commit

---
+++ WidgetTest/WidgetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetTestApp.swift
+//  WidgetTest
+//
+//  Created by sooin on 2023/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WidgetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
